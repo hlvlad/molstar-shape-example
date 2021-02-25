52f7bc3e4d0f5e4e670d06147a860ada8327cf6f
Vladvance <hl.vladyslav@gmail.com>
2021-02-25 12:25:07 +0100

    Cleanup

--- src/Viewer3D.tsx
+++ src/Viewer3D.tsx
@@ -1,12 +1,6 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { MolstarDemoViewer } from "./MolstarDemoViewer";
-// import { RNAspiderTypes as RST } from "./punctures/types";
 
-// type Viewer3DProps = {
-//   model3D: string;
-//   model3DName: string;
-//   report: RST.RNAspiderReportRaw;
-// }
 
 const Viewer3D = () => {
   const viewerRef = useRef<HTMLDivElement>(null);
@@ -20,8 +14,6 @@ const Viewer3D = () => {
 
   useEffect(() => {
     if (molstarPlugin) {
-        // molstarPlugin.loadStructureFromData(model3D, 'pdb', { dataLabel: model3DName });
-        // molstarPlugin.load(report);
         console.log("TEST");
         molstarPlugin.loadFigures();
     }
